41006611dc7ced017213a72f6546cc211c3cf013
f0rtify <jerkstore@my.smccd.edu>
2016-02-09 21:16:25 +0000

    Update lists.js

--- options/lists.js
+++ options/lists.js
@@ -2,7 +2,7 @@
 
 function setURL(){
 	chrome.storage.sync.set({targetlist: document.querySelector('input').value},function(){
-		document.body.innerHTML += "Url: " + document.querySelector('input').value + " has been saved. Now click on the add-on icon and \"shoot the canon\""
+		document.body.innerHTML += "Url: " + document.querySelector('input').value + " has been saved. Now click on the add-on icon and \"NUKE INCOMING""
 	});
 }
 
